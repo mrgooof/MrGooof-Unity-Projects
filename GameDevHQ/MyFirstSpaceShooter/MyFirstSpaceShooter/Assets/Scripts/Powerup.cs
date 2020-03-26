14eeb77f3bba71d32faade919c40c134e5a72325
mrgooof <mrgooof@gmail.com>
2020-03-25 22:13:49 -0400

    Triple_Shot_PowerUp
    Collect
    Timer
    Anim

--- GameDevHQ/MyFirstSpaceShooter/MyFirstSpaceShooter/Assets/Scripts/Powerup.cs
+++ GameDevHQ/MyFirstSpaceShooter/MyFirstSpaceShooter/Assets/Scripts/Powerup.cs
@@ -37,6 +37,15 @@ public class Powerup : MonoBehaviour
     {
         if (other.tag == "Player") 
         {
+
+            Player player = other.transform.GetComponent<Player>();
+
+            if (player !=null)
+            {
+
+                player.TripleShotActive();
+            }
+
             Destroy(this.gameObject);
         }
 
