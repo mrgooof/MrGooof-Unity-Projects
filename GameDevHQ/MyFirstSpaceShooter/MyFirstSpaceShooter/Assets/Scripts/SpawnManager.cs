90251bb068d51d4509fef4bd93a8128c98474751
mrgooof <mrgooof@gmail.com>
2020-04-01 23:33:30 -0400

    First Array & Anim Speed Boost
    Created first array
    Animated speedboost
    Animated Shield boost

--- GameDevHQ/MyFirstSpaceShooter/MyFirstSpaceShooter/Assets/Scripts/SpawnManager.cs
+++ GameDevHQ/MyFirstSpaceShooter/MyFirstSpaceShooter/Assets/Scripts/SpawnManager.cs
@@ -10,7 +10,7 @@ public class SpawnManager : MonoBehaviour
     private GameObject _enemyContainer;
     private bool _stopSpawning = false;
     [SerializeField]
-    private GameObject _tripleShotPowerUpPrefab;
+    private GameObject[] powerups;
     // Start is called before the first frame update
     void Start()
     {
@@ -42,7 +42,8 @@ public class SpawnManager : MonoBehaviour
         while (_stopSpawning == false)
         {
             Vector3 postToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
-            Instantiate(_tripleShotPowerUpPrefab, postToSpawn, Quaternion.identity);
+            int randomPowerUp = Random.Range(0, 2);
+            Instantiate(powerups[randomPowerUp], postToSpawn, Quaternion.identity);
             yield return new WaitForSeconds(Random.Range(3, 8));
         }
     }
